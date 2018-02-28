debfcd2aaacfa797eb04795025362847292c1c06
ZhangXuemeng <Shirley@ZhangXuemengs-MacBook.local>
2018-02-28 17:04:51 -0500

    tackle new functionalities: deleting and creating

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -13,6 +13,26 @@
 // 	});
 // 	}
 // });
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 })
+
+//click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+//add new todo
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grab new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+})
+
